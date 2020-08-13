4d45b0ff662371c469bacf2e0b855573d6462c75
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-13 23:49:26 +0600

    update

---
+++ letconst.js
@@ -0,0 +1,12 @@
+const name = "sourav Bhuiyaan";
+console.log(name);
+
+const num = [12, 26];
+num[0] = 1620;
+console.log(num);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum = sum + i;
+}
+console.log(i);
