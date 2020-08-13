7b4211a4e9df5cf5cb2b5f830f79a0ac213a4cd3
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-14 00:24:32 +0600

    template string

---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "justin";
+const lastName = "Timberlake";
+
+fullName = `${firstName} ${lastName}`;
+
+const multiLine = `sourav
+mourav
+sadaf
+chadap
+abir
+chabir`;
+
+console.log(multiLine);
