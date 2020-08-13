e492774d5bd1a73157508dc45e458a8389cbaacb
alnoman1620 <67537851+alnoman1620@users.noreply.github.com>
2020-08-14 01:54:38 +0600

    class update

---
+++ class.js
@@ -0,0 +1,9 @@
+class Student {
+  constructor(sId, sName) {
+    this.id = sId;
+    this.name = sName;
+  }
+}
+const stu1 = new Student(12, "shuvo");
+const stu2 = new Student(22, "mahiya");
+console.log(stu1, stu2);
